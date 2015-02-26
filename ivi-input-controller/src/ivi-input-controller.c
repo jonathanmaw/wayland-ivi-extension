60dd3bc0e62e4dc94fc4b29d48a1f8384444cace
Jonathan Maw <jonathan.maw@codethink.co.uk>
2015-02-26 15:51:54 +0000

    ivi-input-controller: Implement keyboard grabs

--- ivi-input-controller/src/ivi-input-controller.c
+++ ivi-input-controller/src/ivi-input-controller.c
@@ -63,6 +63,38 @@ static void
 keyboard_grab_key(struct weston_keyboard_grab *grab, uint32_t time,
                   uint32_t key, uint32_t state)
 {
+    struct seat_ctx *seat_ctx = wl_container_of(grab, seat_ctx, keyboard_grab);
+    struct surface_ctx *surf_ctx;
+    struct wl_display *display = grab->keyboard->seat->compositor->wl_display;
+    const struct ivi_controller_interface *interface =
+        seat_ctx->input_ctx->ivi_controller_interface;
+
+    wl_list_for_each(surf_ctx, &seat_ctx->input_ctx->surface_list, link) {
+        struct weston_surface *surface;
+        struct wl_resource *resource;
+        struct wl_client *surface_client;
+        uint32_t serial;
+        if (!(surf_ctx->focus & ILM_INPUT_DEVICE_KEYBOARD))
+            continue;
+
+        /* TODO: Seat acceptance */
+
+        surface = interface->surface_get_weston_surface(surf_ctx->layout_surface);
+        surface_client = wl_resource_get_client(surface->resource);
+        serial = wl_display_next_serial(display);
+
+        wl_resource_for_each(resource, &grab->keyboard->resource_list) {
+            if (wl_resource_get_client(resource) != surface_client)
+                continue;
+            wl_keyboard_send_key(resource, serial, time, key, state);
+        }
+
+        wl_resource_for_each(resource, &grab->keyboard->focus_resource_list) {
+            if (wl_resource_get_client(resource) != surface_client)
+                continue;
+            wl_keyboard_send_key(resource, serial, time, key, state);
+        }
+    }
 }
 
 static void
@@ -70,6 +102,43 @@ keyboard_grab_modifiers(struct weston_keyboard_grab *grab, uint32_t serial,
                         uint32_t mods_depressed, uint32_t mods_latched,
                         uint32_t mods_locked, uint32_t group)
 {
+    struct seat_ctx *seat_ctx = wl_container_of(grab, seat_ctx, keyboard_grab);
+    struct surface_ctx *surf_ctx;
+    struct wl_display *display = grab->keyboard->seat->compositor->wl_display;
+    const struct ivi_controller_interface *interface =
+        seat_ctx->input_ctx->ivi_controller_interface;
+
+    wl_list_for_each(surf_ctx, &seat_ctx->input_ctx->surface_list, link) {
+        struct weston_surface *surface;
+        struct wl_resource *resource;
+        struct wl_client *surface_client;
+        uint32_t serial;
+
+        /* Keyboard modifiers go to surfaces with pointer focus as well */
+        if (!(surf_ctx->focus
+              & (ILM_INPUT_DEVICE_KEYBOARD | ILM_INPUT_DEVICE_POINTER)))
+            continue;
+
+        /* TODO: Seat acceptance */
+
+        surface = interface->surface_get_weston_surface(surf_ctx->layout_surface);
+        surface_client = wl_resource_get_client(surface->resource);
+        serial = wl_display_next_serial(display);
+
+        wl_resource_for_each(resource, &grab->keyboard->resource_list) {
+            if (wl_resource_get_client(resource) != surface_client)
+                continue;
+            wl_keyboard_send_modifiers(resource, serial, mods_depressed,
+			               mods_latched, mods_locked, group);
+        }
+
+        wl_resource_for_each(resource, &grab->keyboard->focus_resource_list) {
+            if (wl_resource_get_client(resource) != surface_client)
+                continue;
+            wl_keyboard_send_modifiers(resource, serial, mods_depressed,
+			               mods_latched, mods_locked, group);
+        }
+    }
 }
 
 static void
